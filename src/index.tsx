fff0d5b7f690d71faa1906f19f8e0864ab34af3b
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-09-27 11:23:53 +0330

    Add plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
 
-    const onClick = () => {
-      console.log(input)
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: "/esbuild.wasm",
+        });
     };
+
+    const onClick =async() => {
+        if (!ref.current) {
+            return;
+        }
+        const result = await ref.current.transform(input, {
+            loader: "jsx",
+            target: "es2015",
+        });
+
+        setCode(result.code)
+
+    };
+
+    useEffect(() => {
+        startService();
+    }, []);
 
     return (
         <div>
