f3e7f2261ec6f40303a47991ae019762ab6cf78b
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-09-27 17:11:28 +0330

    Handle NODE_ENV Warning

--- src/index.tsx
+++ src/index.tsx
@@ -15,21 +15,24 @@ const App = () => {
         });
     };
 
-    const onClick =async() => {
+    const onClick = async () => {
         if (!ref.current) {
             return;
         }
         const result = await ref.current.build({
-            entryPoints: ['index.js'],
+            entryPoints: ["index.js"],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()]
+            plugins: [unpkgPathPlugin()],
+            define: {
+                "process.env.NODE_ENV": '"production"',
+                global: "window",
+            },
         });
 
         // console.log(result)
 
-        setCode(result.outputFiles[0].text)
-
+        setCode(result.outputFiles[0].text);
     };
 
     useEffect(() => {
