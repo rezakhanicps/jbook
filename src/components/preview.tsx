537a98e164971a36122159d8019d2d476fdf7669
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-09 00:14:32 +0330

    Handling Async Error

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -8,21 +8,31 @@ interface PreviewProps {
 const html = `
 <html>
     <head>
-    <style> html { background-color: white;} </style>
+    <style>html { background-color: white; }</style>
     </head>
     <body>
-        <div id="root"></div>
-        <script>
-            window.addEventListener('message', (event)=>{
-                try{
-                    eval(event.data)
-                }catch (error){ 
-                    const root = document.querySelector('#root');
-                    root.innerHTML = '<div style="color: red;">'+ error + '</div>'
-                    throw error
-                }
-            }, false);
-        </script>
+    <div id="root"></div>
+    <script>
+        const handleError = (err) => {
+        const root = document.querySelector('#root');
+        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+        console.error(err);
+        };
+
+        window.addEventListener('error', (event) => {
+        event.preventDefault();
+        console.log(event);
+        handleError(event.error);
+        });
+
+        window.addEventListener('message', (event) => {
+        try {
+            eval(event.data);
+        } catch (err) {
+            handleError(err);
+        }
+        }, false);
+    </script>
     </body>
 </html>
 `;
