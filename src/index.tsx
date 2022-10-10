25fb424215022065355016d6dc9241816d3c7024
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-10 22:50:45 +0330

    Add state(redux)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import "bulmaswatch/superhero/bulmaswatch.min.css";
-import ReactDOM from "react-dom";
-import CodeCell from "./components/code-cell";
-import TextEditor from "./components/text-editor";
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import TextEditor from './components/text-editor';
+import { store } from './state';
 
 const App = () => {
     return (
-        <div>
-            <TextEditor />
-        </div>
+        <Provider store={store}>
+            <div>
+                <TextEditor />
+            </div>
+        </Provider>
     );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
