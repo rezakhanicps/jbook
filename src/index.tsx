19bfb61ce73d578d4aecb6d45121a0961eaaf27e
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-17 13:52:19 +0330

    Applying custom css

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import CellList from './components/cell-list';
-import TextEditor from './components/text-editor';
 import { store } from './state';
 
 const App = () => {
