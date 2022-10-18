ec2c1c9d8f550f705d4cb5b2535728ffa083158e
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-18 15:04:52 +0330

    Adding Eager Bundling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
-import bundler from '../bundler';
 import Resizable from './resizable';
 import { Cell } from '../state';
 import { useAction } from '../hooks/use-actions';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 interface CodeCellProps {
     cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-    const [code, setCode] = useState('');
-    const [err, setErr] = useState('');
-    const { updateCell } = useAction();
+    const { updateCell, createBundle } = useAction();
+
+    const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
     useEffect(() => {
+        if (!bundle) {
+            createBundle(cell.id, cell.content);
+            return;
+        }
+
         const timer = setTimeout(async () => {
-            const output = await bundler(cell.content);
-            setCode(output.code);
-            setErr(output.err);
+            createBundle(cell.id, cell.content);
         }, 1000);
 
         return () => {
             clearTimeout(timer);
         };
-    }, [cell.content]);
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [cell.content, cell.id, createBundle]);
 
     return (
         <Resizable direction="vertical">
@@ -42,7 +47,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-                <Preview code={code} err={err} />
+                {bundle && <Preview code={bundle.code} err={bundle.err} />}
             </div>
         </Resizable>
     );
