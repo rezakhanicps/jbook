9eab6fb621b08d098cf3c34657979f207d3dc11d
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-18 22:23:07 +0330

    Implementing a Show Function

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,6 +1,6 @@
-import * as esbuild from "esbuild-wasm";
-import { fetchPlugin } from "./plugins/fetch-plugin";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import * as esbuild from 'esbuild-wasm';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 let service: esbuild.Service;
 
@@ -8,28 +8,30 @@ const bundle = async (rawCode: string) => {
     if (!service) {
         service = await esbuild.startService({
             worker: true,
-            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
         });
     }
 
     try {
         const result = await service.build({
-            entryPoints: ["index.js"],
+            entryPoints: ['index.js'],
             bundle: true,
             write: false,
             plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
             define: {
-                "process.env.NODE_ENV": '"production"',
-                global: "window",
+                'process.env.NODE_ENV': '"production"',
+                global: 'window',
             },
+            jsxFactory: '_React.createElement',
+            jsxFragment: '_React.Fragment',
         });
         return {
             code: result.outputFiles[0].text,
-            err: "",
+            err: '',
         };
     } catch (error: any) {
         return {
-            code: "",
+            code: '',
             err: error.message,
         };
     }
--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,6 +6,7 @@ import Resizable from './resizable';
 import { Cell } from '../state';
 import { useAction } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 
 interface CodeCellProps {
     cell: Cell;
@@ -14,46 +15,16 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { updateCell, createBundle } = useAction();
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-    const cumulativeCode = useTypedSelector((state) => {
-        const { data, order } = state.cells;
-        const orderedCells = order.map((id) => data[id]);
-        const cumulativeCode = [
-            `
-            import React from 'react';
-            import ReactDOM from 'react-dom';
-            const show = (value)=>{
-                const root = document.querySelector('#root');
-                if(typeof value === 'object'){
-                    if(value.$$typeof && value.props) {
-                        ReactDOM.render(value, root);
-                    }else {
-                        root.innerHTML = JSON.stringify(value);
-                    }
-                }else {
-                    root.innerHTML = value;
-                }
-            };
-            `,
-        ];
-        for (const c of orderedCells) {
-            if (c.type === 'code') {
-                cumulativeCode.push(c.content);
-            }
-            if (c.id === cell.id) {
-                break;
-            }
-        }
-        return cumulativeCode;
-    });
+    const cumulativeCode = useCumulativeCode(cell.id);
 
     useEffect(() => {
         if (!bundle) {
-            createBundle(cell.id, cumulativeCode.join('\n'));
+            createBundle(cell.id, cumulativeCode);
             return;
         }
 
         const timer = setTimeout(async () => {
-            createBundle(cell.id, cumulativeCode.join('\n'));
+            createBundle(cell.id, cumulativeCode);
         }, 1000);
 
         return () => {
@@ -61,7 +32,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         };
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cumulativeCode.join('\n'), cell.id, createBundle]);
+    }, [cumulativeCode, cell.id, createBundle]);
 
     return (
         <Resizable direction="vertical">
